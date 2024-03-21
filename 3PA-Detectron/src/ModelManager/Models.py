de0c6c55160417cda9abeacc7307e38aa7a8c8a9
lyna1404 <jl_chikouche@esi.dz>
2024-03-21 08:10:33 -0400

    ModelManager : Completed, documented and tested

--- 3PA-Detectron/src/ModelManager/Models.py
+++ 3PA-Detectron/src/ModelManager/Models.py
@@ -1,68 +1,107 @@
 import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
+from DataPreparingStrategy import ToDmatrixStrategy
 
 class Model:
+    """
+    Abstract base class for models. Defines the structure that all models should follow.
+    """
     def train(self, X, y):
-        pass
+        """
+        Trains the model on the given dataset.
+
+        :param X: Features for training.
+        :param y: Labels for training.
+        :raises:
+            NotImplementedError: If the subclass has not implemented this method.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
 
     def predict(self, X):
-        pass
+        """
+        Makes predictions using the model for the given input.
+
+        :param X: Features for prediction.
+        :return: Predictions made by the model.
+        :raises:
+            NotImplementedError: If the subclass has not implemented this method.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
 
 class XGBoostModel(Model):
+    """
+    A concrete implementation of the Model class for XGBoost models.
+    """
     def __init__(self, params_or_model=None, model_class=None):
         """
-        Initialize the XGBoostModel using params or a loaded pickled model.
+        Initializes the XGBoostModel either with parameters for a new model or a loaded pickled model.
+
         :param params_or_model: Either a dictionary of parameters for the booster model or a loaded pickled model.
-        :param model_class: Class of the model if a pickled model is provided.
+        :param model_class: Specifies the class of the model if a pickled model is provided. Defaults to xgb.Booster if None.
         """
         self.params = None
         self.model = None
         self.model_class = None
         self.pickled_model = False
-        
-        if model_class is None : 
+        self.data_preparation_strategy = ToDmatrixStrategy()
+
+        if model_class is None:  # The model is initialized using parameters
             self.params = params_or_model
             self.model_class = xgb.Booster
-            self.pickled_model = False
-        else:  
+        else:  # A pickled model is provided
             self.model = params_or_model
             self.model_class = model_class
             self.pickled_model = True
 
-    def train(self, X, y, num_boost_round=10):
+    def _ensure_dmatrix(self, features, labels=None):
         """
-        Train the model.
-        :param X: Feature matrix as a numpy array or a DMatrix.
-        :param y: Labels as a numpy array.
-        :param num_boost_round: Number of boosting rounds.
+        Ensures that the input data is converted to a DMatrix format, using the defined data preparation strategy.
+
+        :param features: Features array.
+        :param labels: Labels array, optional.
+        :return: A DMatrix object.
         """
-        dtrain = xgb.DMatrix(X, label=y)
-        self.model = xgb.train(self.params, dtrain, num_boost_round=num_boost_round)
+        return self.data_preparation_strategy.execute(features, labels) if not isinstance(features, xgb.DMatrix) else features
+
+    def train(self, X, y):
+        """
+        Trains the model on the provided dataset.
+
+        :param X: Features for training.
+        :param y: Labels for training.
+        :raises
+            ValueError: If parameters for xgb.Booster are not initialized before training.
+            NotImplementedError: If the model_class is not supported for training.
+        """
+        
+        if self.model_class is xgb.Booster: 
+            if not self.params:
+                raise ValueError("Parameters for xgb.Booster must be initialized before training.")
+            dtrain = self._ensure_dmatrix(X, y)
+            self.model = xgb.train(self.params, dtrain)
+        elif self.model_class is xgb.XGBClassifier: 
+            self.model.fit(X, y)
+        else:
+            raise NotImplementedError(f"Training not implemented for model class {self.model_class}")
 
     def predict(self, X):
         """
-        Make predictions with the trained model.
-        :param X: Feature matrix as a numpy array or a DMatrix.
-        :return: A numpy array of predictions.
+        Makes predictions with the model for the given input.
+
+        :param X: Features for prediction.
+        :return: Predictions made by the model.
+        :raises
+            ValueError: If the model has not been trained.
+            NotImplementedError: If the model_class is not supported for prediction.
         """
-        dtest = xgb.DMatrix(X)
-        return self.model.predict(dtest)
-
-class BaseModelManager(Model):
-    __baseModel = None
-
-    @classmethod
-    def set_base_model(cls, model):
-        if cls.__baseModel is None:
-            cls.__baseModel = model
+        if self.model_class is xgb.Booster:
+            if self.model is None:
+                raise ValueError("The xgb.Booster model has not been trained.")
+            dtest = self._ensure_dmatrix(X)
+            return self.model.predict(dtest)
+        elif self.model_class is xgb.XGBClassifier:
+            if self.model is None:
+                raise ValueError("The XGBClassifier model has not been instantiated or trained.")
+            return self.model.predict(X)
         else:
-            raise TypeError("The Base Model has already been initialized")
-
-    @classmethod
-    def get_instance(cls):
-        if cls.__baseModel is None:
-            raise TypeError("The Base Model has not been initialized yet")
-        else:
-            return cls.__baseModel
-
-
+            raise NotImplementedError(f"Prediction not implemented for model class {self.model_class}")
--- 3PA-Detectron/src/ModelManager/testing.py
+++ 3PA-Detectron/src/ModelManager/testing.py
@@ -1,7 +1,10 @@
 import ModelFactories
 import Models
+import BaseModel
 import xgboost as xgb
 import pickle
+import numpy as np
+from xgboost.sklearn import XGBClassifier
 
 """loaded_model = xgboost.Booster()
 loaded_model.load_model('uci_heart_0.model')
@@ -18,7 +21,7 @@ if isinstance(model, xgb.Booster):
     print('true')
 else : 
     print('no')"""
-
+"""
 print("Model with params exemple")
 params = {'objective': 'binary:logitraw'}
 fact = ModelFactories.ModelFactory()
@@ -39,9 +42,34 @@ print("BaseModel setting exemple")
 fact = ModelFactories.ModelFactory()
 model = fact.create_model_from_pickled('pickled_model.pkl')
 # print(Models.BaseModelManager.get_instance())
-Models.BaseModelManager.set_base_model(model)
-print(Models.BaseModelManager.get_instance())
+BaseModel.BaseModelManager.set_base_model(model)
+print(BaseModel.BaseModelManager.get_instance().model)
+# cloned_model = type(BaseModel.BaseModelManager.get_instance())()
+# print(type(cloned_model))
 # Models.BaseModelManager.set_base_model(model)
+cloned_model = BaseModel.BaseModelManager.clone_base_model()
+print(cloned_model.model)
 
+"""
 
+# Prepare training data
+features = np.array([[1, 2], [3, 4]])
+labels = np.array([1, 0])
+dtrain = xgb.DMatrix(features, label=labels)
 
+# Train xgb.Booster
+params = {'objective': 'binary:logistic', 'use_label_encoder': False}
+num_rounds = 10
+booster_model = xgb.train(params, dtrain, num_rounds)
+
+# Pickle the xgb.Booster model
+with open('booster_model.pkl', 'wb') as file:
+    pickle.dump(booster_model, file)
+
+# Train xgb.XGBClassifier
+classifier_model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+classifier_model.fit(features, labels)
+
+# Pickle the xgb.XGBClassifier model
+with open('classifier_model.pkl', 'wb') as file:
+    pickle.dump(classifier_model, file)
